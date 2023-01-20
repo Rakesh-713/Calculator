17f4eccf8ae64993f9b997b18d201dcc5895a2a5
Rakesh-713 <gundarakesh713@gmail.com>
2023-01-21 01:27:01 +0530

    react app created files

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <input type="numbers" />
+      </center>
+    </div>
+  );
+}
+
+export default App;
